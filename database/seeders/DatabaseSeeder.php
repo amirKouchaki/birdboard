da88cbe9e2606ebd78d608d4c7f1e0a9a00b8845
amir kouchaki <amirkouchaki1@gmail.com>
2021-09-20 12:16:53 +0430

    database seeder fix 1

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -5,6 +5,7 @@ namespace Database\Seeders;
 use App\Models\User;
 use Illuminate\Database\Seeder;
 use Illuminate\Support\Facades\Hash;
+use Illuminate\Testing\Fluent\Concerns\Has;
 
 class DatabaseSeeder extends Seeder
 {
@@ -16,6 +17,6 @@ class DatabaseSeeder extends Seeder
     public function run()
     {
         User::factory()->create(['email' => 'test@gmail.com']);
-        User::factory()->create(['email' => Hash::make('12345678')]);
+        User::factory()->create(['email' => 'test@test.com','password' => Hash::make(12345678)]);
     }
 }
